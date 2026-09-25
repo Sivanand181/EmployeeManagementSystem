6feafbb7fab55909722900012b1b522b90b0e25d
Sivanand181 <sivanand.chandra@gmail.com>
2026-09-25 10:10:53 -0500

    Add EmployeeManagement.API/Program.cs

---
+++ EmployeeManagement.API/Program.cs
@@ -0,0 +1,18 @@
+using EmployeeManagement.API.Data;
+using EmployeeManagement.API.Repositories;
+using EmployeeManagement.API.Services;
+using EmployeeManagement.API.Middleware;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<AppDbContext>(o => o.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddScoped<IEmployeeRepository, EmployeeRepository>();
+builder.Services.AddScoped<IEmployeeService, EmployeeService>();
+var app = builder.Build();
+using (var scope = app.Services.CreateScope()) { var db = scope.ServiceProvider.GetRequiredService<AppDbContext>(); db.Database.EnsureCreated(); SeedData.Initialize(db); }
+app.UseMiddleware<ExceptionMiddleware>();
+app.UseSwagger(); app.UseSwaggerUI(); app.UseHttpsRedirection(); app.MapControllers(); app.Run();
+public partial class Program { }
