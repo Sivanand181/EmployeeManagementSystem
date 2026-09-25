6e1ce6cffb3004db49fe12f4f8f00d59de104bb2
Sivanand181 <sivanand.chandra@gmail.com>
2026-09-25 10:11:04 -0500

    Add EmployeeManagement.API/Repositories/IEmployeeRepository.cs

---
+++ EmployeeManagement.API/Repositories/IEmployeeRepository.cs
@@ -0,0 +1,6 @@
+using EmployeeManagement.API.Models;
+namespace EmployeeManagement.API.Repositories;
+public interface IEmployeeRepository {
+ Task<List<Employee>> GetAllAsync(); Task<Employee?> GetByIdAsync(int id);
+ Task<Employee> AddAsync(Employee employee); Task UpdateAsync(Employee employee); Task DeleteAsync(Employee employee);
+}
